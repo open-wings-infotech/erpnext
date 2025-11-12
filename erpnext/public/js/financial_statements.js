b1568e025311eda524e97c54d5ddc0669e79f5c6
Vinod Nadar <nadarvinod@live.com>
2025-11-12 19:20:39 +0000

    Financial Statements - Set Default Financial Year

--- erpnext/public/js/financial_statements.js
+++ erpnext/public/js/financial_statements.js
@@ -206,6 +206,7 @@ function get_filters() {
 			options: "Fiscal Year",
 			reqd: 1,
 			depends_on: "eval:doc.filter_based_on == 'Fiscal Year'",
+			default: erpnext.utils.get_fiscal_year(date=frappe.datetime.get_today(), company=frappe.query_report.get_filter_value("company")),
 			get_query: function () {
 				var company = frappe.query_report.get_filter_value("company");
 				return {
@@ -222,6 +223,7 @@ function get_filters() {
 			options: "Fiscal Year",
 			reqd: 1,
 			depends_on: "eval:doc.filter_based_on == 'Fiscal Year'",
+			default: erpnext.utils.get_fiscal_year(date=frappe.datetime.get_today(), company=frappe.query_report.get_filter_value("company")),
 			get_query: function () {
 				var company = frappe.query_report.get_filter_value("company");
 				return {
